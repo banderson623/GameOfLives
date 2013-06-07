3eaa26a21734a54f2ed74e3e7220c93712ff6f07
Brian Anderson <banderson623@gmail.com>
2013-06-07 09:09:11 -0500

    switched to structs so much nicer

--- c/game.c
+++ c/game.c
@@ -10,42 +10,49 @@
 #define OK 0
 #define FAIL 1
 
-int releaseBoard(int height, int** board);
-void printBoard(int width, int height, int** board);
-int** generateBoard(int width, int height, int seed);
+// I've been using C++ too long :)
+typedef struct {
+  int height;
+  int width;
+  int** board;  
+} Board;
+
+int releaseBoard(Board board);
+void printBoard(Board board);
+Board generateBoard(int width, int height, int seed);
 
 int main (int argc, char const *argv[]){    
     int height = DEFAULT_HEIGHT;
     int width =  DEFAULT_WIDTH;
     
     // generate the board
-    int** board = generateBoard(width, height, 0);
+    Board board = generateBoard(width, height, 0);
     
     // print the current board
-    printBoard(width,height,board);
+    printBoard(board);
     
     // Now release the memory
-    releaseBoard(height,board);
+    releaseBoard(board);
     
     return OK;
 }
 
 
 // Release the board of height (rows)
-int releaseBoard(int height, int** board){
-    for (int row = 0; row < height; ++row){
+int releaseBoard(Board board){
+    for (int row = 0; row < board.height; ++row){
         // Walk DOWN the board releasing
-        free(board[row]);  
+        free(board.board[row]);  
     }  
-    free(board);
+    free(board.board);
     return OK;
 }
 
 // Print the board
-void printBoard(int width, int height, int** board){
-    for(int row = 0; row < height ; ++row){
-        for(int column = 0; column < width; ++column){
-            printf("%c",(board[row][column] == 1) ? '*' : ' ');
+void printBoard(Board board){
+    for(int row = 0; row < board.height ; ++row){
+        for(int column = 0; column < board.width; ++column){
+            printf("%c",(board.board[row][column] == 1) ? '*' : ' ');
         }
         printf("\n");
     }
@@ -54,21 +61,25 @@ void printBoard(int width, int height, int** board){
 // Generates a board with width and height dimensions
 // in the 2d array board
 // the seed, seeds the random number generator
-int** generateBoard(int width, int height, int seed) {
+Board generateBoard(int width, int height, int seed) {
     // seed the random number generator
     srand(seed);
     
+    Board board;
+    board.width = width;
+    board.height = height;
+        
     // allocate the space, for the rows, to hold an array of integers
-    int** board = (int**) malloc(height * sizeof(int*));
+    board.board = (int**) malloc(height * sizeof(int*));
     
     for(int row = 0; row < height ; ++row){
 
         // Allocate enough size in this board for each row
-        board[row] = (int*) malloc(width * sizeof(int));
+        board.board[row] = (int*) malloc(width * sizeof(int));
 
         for(int column = 0; column < width; ++column){
             // 1 is alive, 0 is dead
-            board[row][column] = rand() % 2;
+            board.board[row][column] = rand() % 2;
         }
     }
     
